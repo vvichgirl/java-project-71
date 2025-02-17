e68c101f4bd39bab0746e347614440d02921af7c
Viktoriia Zubkova <vichgirl@gmail.com>
2025-02-17 13:49:23 +0300

    add picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ application {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("info.picocli:picocli:4.7.6")
 }
 
 tasks.test {
